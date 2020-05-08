b07597c1e6b8289e2758c6cd65225a09e9af669a
Yuxuan Hu <57782783+yuxuan-hu@users.noreply.github.com>
2020-05-08 22:31:17 +0800

    first commit

---
+++ vmware.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env zsh
+local file_given
+file_given=false
+if [ -z $2 ]
+  then
+    vmfile=$2
+  else
+    vmfile=${VMFILE}
+fi
+function check_file {
+  if [ -z ${vmfile} ]
+    then
+      echo "Usage: either pass the path to .vmx file as an argument or set the environment variable VMFILE"
+      exit 1
+  fi
+}
+case $1 in
+  start)
+    check_file
+    vmrun start ${vmfile} nogui
+    ;;
+  stop)
+    check_file
+    vmrun stop ${vmfile} nogui
+    ;;
+  suspend)
+    check_file
+    vmrun suspend ${vmfile} nogui
+    ;;
+  pause)
+    check_file
+    vmrun pause ${vmfile} nogui
+    ;;
+  unpause)
+    check_file
+    vmrun unpause ${vmfile} nogui
+    ;;
+  reset)
+    check_file
+    vmrun reset ${vmfile} nogui
+    ;;
+  status)
+    if [ -z $2 ]
+      then
+        vmrun list
+      else
+        vmrun list | grep $2
+    fi
+    ;;
+  *)
+    echo "Usage: possible options are: start | stop | suspend | pause | unpause | reset | status"
+    exit 1
+esac
